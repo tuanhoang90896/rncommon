2fc856ed0290ac7311350763ab9f2ddea54bdee7
Hoang Tue <kingwisdom.dev@gmail.com>
2021-07-02 14:59:21 +0700

    Update module facebook login

--- src/router/navigation.js
+++ src/router/navigation.js
@@ -3,6 +3,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../screen/home/components';
 import TueDev from '../screen/tue_dev/components';
+import GoogleLogin from "../screen/common/googleLogin/components";
+import FacebookLogin from "../screen/common/facebookLogin/components";
+import PushNotification from "../screen/common/facebookLogin/components";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +14,9 @@ function AppStack() {
         <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="TueDev" component={TueDev} />
+            <Stack.Screen name="GoogleLogin" component={GoogleLogin} />
+            <Stack.Screen name="FacebookLogin" component={FacebookLogin} />
+            <Stack.Screen name="PushNotification" component={PushNotification} />
         </Stack.Navigator>
     )
 
--- src/screen/tue_dev/components/index.js
+++ src/screen/tue_dev/components/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { View } from "react-native";
-// import { Button } from "react-native-elements";
+import { Button } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
-import GoogleLogin from "../../common/googleLogin/components";
-
 
 const TueDev = (props) => {
     const navigation = useNavigation();
@@ -11,7 +9,36 @@ const TueDev = (props) => {
 
     return (
         <View style={{ flex: 1, padding: 16 }}>
-            <GoogleLogin/>
+            <View style={{ padding: 16 }}>
+                <Button
+                    onPress={() => {
+                        navigation.navigate('GoogleLogin')
+                    }}
+                    buttonStyle={{ width: '100%' }}
+                    title="Google Login"
+                    type="outline"
+                />
+            </View>
+            <View style={{ padding: 16 }}>
+                <Button
+                    onPress={() => {
+                        navigation.navigate('FacebookLogin')
+                    }}
+                    buttonStyle={{ width: '100%' }}
+                    title="Facebook Login"
+                    type="outline"
+                />
+            </View>
+            <View style={{ padding: 16 }}>
+                <Button
+                    onPress={() => {
+                        navigation.navigate('PushNotification')
+                    }}
+                    buttonStyle={{ width: '100%' }}
+                    title="Push Notification"
+                    type="outline"
+                />
+            </View>
         </View>
     );
 };
