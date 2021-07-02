54b5a669e3c03802d39ebb0b1bb87c75fe849d15
Bim1604 <dangvipro3@gmail.com>
2021-07-02 17:27:34 +0700

    second commit

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppStack from './src/router/navigation';
+import AppRouter from './src/router/navigation';
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <AppStack />
+        <AppRouter />
       </NavigationContainer>
     </SafeAreaProvider>
   );
